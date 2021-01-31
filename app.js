74eb0ba978ca102ea95dc9ba161c737892fbbe48
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-01-31 23:28:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express') 
+//Mot clé "require" indique à nodeJS d'aller chercher
+// la dependance 'express' ds le folder node module
+  
+const app = express() //creat° d'une instance de l'appli express grace à la METHOD du meme nom
+const port = 3000 // port sur lequel on va demarrer notre APIREST pr la suite
+  
+app.get('/', (req, res) => res.send('Hello, Express 👋!'))
+//Notre 1er poitn de terminaison definit
+//METHOD de la req GET qui va prendre en param 2 elements...
+  
+app.listen(port, () => 
+    console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
+//METHOD listen fournit pr express
