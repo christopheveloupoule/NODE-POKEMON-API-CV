33958050f7abb6ed4c6d3a52cec834f7c2c89ea7
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-01-31 23:52:16 +0100

    use req params for url

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const port = 3000 // port sur lequel on va demarrer notre APIREST pr la suite
 app.get('/', (req, res) => res.send('Hello, Express 👋!'))
 //Notre 1er poitn de terminaison definit
 //METHOD de la req GET qui va prendre en param 2 elements...
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id //recupere l'id ds l'url & send la res au client
+    res.send(`Vous avez demandé le pokémon n°${id}`)
+})
   
 app.listen(port, () => 
     console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
