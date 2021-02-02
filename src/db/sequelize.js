493bace1876693b12f032357a9869226eda46f19
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-02-03 00:03:20 +0100

    encrytage du pwd via le hash

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,6 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize')
 const PokemonModel = require('../models/pokemon')
 const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
+const bcrypt = require('bcrypt')
   
 const sequelize = new Sequelize('pokedex', 'root', '', {
   host: 'localhost',
@@ -27,6 +28,14 @@ const initDb = () => {
         types: pokemon.types //.join() //plus besoin de join grace au getter & setter
       }).then(pokemon => console.log(pokemon.toJSON()))
     })
+
+//Fonctionnement de la METHOD hash,en param pwd + hash
+    bcrypt.hash('pikachu', 10)
+//une fois la METHOD hash appelée, on recupe le pwd crypté
+    .then(hash => User.create({ username: 'pikachu', password: hash }))
+    .then(user => console.log(user.toJSON()))
+
+    console.log('La base de donnée a bien été initialisée !')
 
 //METHOD create, on pousse un nouveau user ds la db
     User.create({
