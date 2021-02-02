f0ab3f6966979f3a44970aeeebada027c6c2350f
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-02-02 19:28:28 +0100

    mise en place du validateur personnalisé

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,3 +1,7 @@
+//Def des différentes typesqui peuvent être à un pokemon
+const validTypes = ['Plante', 'Poison', 'Feu', 'Eau', 'Insecte', 'Vol', 'Normal', 'Electrik', 'Fée']
+
+
 /*export d'une fct a 2params, sequelize (obj) connec° a la DB,
 l'interet de cette{},il a une propriete 'define' qui permet 
 de declarer un nouveau model aupres de sequelize*/
@@ -18,14 +22,6 @@ module.exports = (sequelize, DataTypes) => {
         validate: { // validateur
           notEmpty: { msg: 'Le nom ne peut pas être vide.' },
           notNull: { msg: 'Le nom est une propriété requise.'},
-          min: {
-            args: [0],
-            msg: 'Les points de vie doivent être supérieurs ou égales à 0.'
-          },
-          max: {
-            args: [999],
-            msg: 'Les points de vie doivent être inférieures ou égales à 999.'
-          },
         }
       },
       hp: {
@@ -36,11 +32,11 @@ module.exports = (sequelize, DataTypes) => {
           notNull: { msg: 'Les points de vie sont une propriété requise.'},
           min: {
             args: [0],
-            msg: 'Les points de dégâts doivent être supérieurs ou égales à 0.'
+            msg: 'Les points de vie doivent être supérieurs ou égales à 0.'
           },
           max: {
-            args: [99],
-            msg: 'Les points de dégâts doivent être inférieures ou égales à 99.'
+            args: [999],
+            msg: 'Les points de vie doivent être inférieures ou égales à 999.'
           },
         }
       },
@@ -49,7 +45,15 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         validate: { // validateur
           isInt: { msg: 'Utilisez uniquement des nombres entiers pour les points de dégât.' },
-          notNull: { msg: 'Les points de dégâts sont une propriété requise.'}
+          notNull: { msg: 'Les points de dégâts sont une propriété requise.'},
+          min: {
+            args: [0],
+            msg: 'Les points de dégâts doivent être supérieurs ou égales à 0.'
+          },
+          max: {
+            args: [99],
+            msg: 'Les points de dégâts doivent être inférieures ou égales à 99.'
+          },
         }
       },
       picture: {
@@ -68,7 +72,24 @@ module.exports = (sequelize, DataTypes) => {
         },
         set(types) { // a l'inverse le setter, transformer le arr ds types de Pokemon en str unique(API=>DB)
           this.setDataValue('types', types.join())
-        }
+        },
+        validate: { //Validateur personnalisé sur mesure!!
+          isTypesValid(value) { //fonction de nom arbitraire
+            if(!value) { /*en param la valeur value de la propriete types contenu ds la db
+sans prendre en compte le getter ou le setter de cette propr*/
+              throw new Error('Un pokémon doit au moins avoir un type.')
+            }
+            if(value.split(',').length > 3) {
+              throw new Error('Un pokémon ne peux pas avoir plus de trois types.')
+            }
+            value.split(',').forEach(type => { /*on verife chacun ds types d'un pok
+pr savoir si il est bien inclu ds la liste de pok*/
+              if(!validTypes.includes(type)) {
+                throw new Error(`Le type d'un pokémon doit appartenir à la liste suivante : ${validTypes}`)
+              }
+            });
+          }
+        }  
       }
     }, {//Option de parametrage global
       timestamps: true, //Modofier le comportement par defaut de sequelize
