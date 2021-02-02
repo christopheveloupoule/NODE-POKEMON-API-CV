6eda1d0ca94eecf3135b26f226472d8e968200ee
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-02-03 00:25:19 +0100

    user et pwd verif avant authentification

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,13 +5,26 @@ module.exports = (app) => {
   app.post('/api/login', (req, res) => {
 //METHOD 'findOne' : req pr determiner les infos d'un user
     User.findOne({ where: { username: req.body.username } }).then(user => {
-//MEETHODE 'compare' du module 'bcrypt' pwd user au pwd de la db        
-      bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
-        if(isPasswordValid) {
-          const message = `L'utilisateur a été connecté avec succès`;
-          return res.json({ message, data: user })
+
+        if(!user) { //ident
+            const message = `L'utilisateur demandé n'existe pas.`
+            return res.status(404).json({ message })
         }
+
+//METHOD 'compare' du module 'bcrypt' pwd user au pwd de la db        
+      bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+        if(!isPasswordValid) { //eventualite de mot de passe erroné
+            const message = `Le mot de passe est incorrect.`
+            return res.status(401).json({message})
+          }
+
+        const message = `L'utilisateur a été connecté avec succès`;
+        return res.json({ message, data: user, token })
       })
+    })
+    .catch(error => { //cas d'erreur générique, appel réseau qui echouerait par exemple
+        const message = `L'utilisateur n'a pas pu être connecté. Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
     })
   })
 }
