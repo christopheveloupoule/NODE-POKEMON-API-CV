167e6ffe382854e29c0b169494522af028ee30a0
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-02-02 16:22:30 +0100

    Gestion ds err & utilisation de la METHOD POST

--- src/routes/createPokemon.js
+++ src/routes/createPokemon.js
@@ -7,5 +7,10 @@ module.exports = (app) => {
         const message = `Le pokémon ${req.body.name} a bien été crée.`
         res.json({ message, data: pokemon })
       })
+      .catch(error => { //req sequelize echoue
+        const message = `La liste des pokémons n'a pas pu être ajouté. 
+                         Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
+      })
   })
 }
